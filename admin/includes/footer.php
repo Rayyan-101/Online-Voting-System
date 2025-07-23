869918c8cccaa542630714faa0135e96adc96e32
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-07-23 16:54:35 +0530

    Adding project files

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Online Voting System</b>
+    </div>
+    <strong> &copy; Powered By <a href="#">MOHAMMED ZOHEB</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b style="font-size: 18px; font-weight: bold;">Online Voting System</b>
+      </div>
+      <strong>
+  <span style="font-size: 18px; font-weight: bold;">Powered by</span> 
+  <a href="#" style="font-size: 20px;">MOHAMMED ZOHEB</a>
+</strong>
+
+    </div>
+    <!-- /.container -->
+</footer>
